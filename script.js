1f03e890bb471629f904e2c7226182e2fa8b0f12
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2025-11-15 02:06:20 -0800

    add style email

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 const apriUrlapi = "https://flynn.boolean.careers/exercises/api/random/mail"
 // console.log("ciao")
 const titleElem = document.querySelector("h1")
-const btn = document.querySelector(".reset")
+const btnGenera = document.querySelector(".reset")
 const form = document.querySelector("form")
+const submitBtn = document.getElementById("submit")
+const listEmail = document.querySelector(".list-style")
 let isGeneerate = false;
 axios
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then(function (resp) {
 
-        btn.addEventListener("click", function () {
+
+        btnGenera.addEventListener("click", function () {
 
             if (isGeneerate === true) {
-                
-
             } else {
-                btn.innerHTML = "genera"
+                submitBtn.classList.remove("d-none")
+                btnGenera.classList.add("d-none")
                 for (let i = 0; i < 10; i++) {
                     const email = resp.data.response;
                     let newEmail = [];
                     newEmail.push(email);
-                    console.log(newEmail)
                     const emailStamp = document.createElement("ul")
-                    emailStamp.innerHTML = `<li>${email}</li>`;
+                    emailStamp.innerHTML =
+                        `<ul class="m-3 p-3 text-white bg-info bg-opacity-10 border border-white border-start-0 rounded-end">
+                           <li>${email} </li>
+                         </ul>`;
                     document.body.appendChild(emailStamp);
 
                 }
             }
-isGeneerate = !isGeneerate
+            isGeneerate = !isGeneerate
 
         })
 
