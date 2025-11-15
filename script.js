8f558038d0ea78b6f3a5a5f3a660e5bfc41eb818
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2025-11-15 01:14:19 -0800

    add button stampa email

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const apriUrlapi = "https://flynn.boolean.careers/exercises/api/random/mail"
 // console.log("ciao")
 const titleElem = document.querySelector("h1")
+const btn = document.querySelector(".reset")
+const form = document.querySelector("form")
+let isGeneerate = false;
 axios
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then(function (resp) {
-        let email = resp.data.response;
-        titleElem.innerHTML = `${email}`
-        let newEmail = [];
-        
-        for(let i = 0; i < 10; i++){
-            
-            newEmail.push(email);
-            console.log(newEmail)
-            const emailStamp = document.createElement("ul")
-            emailStamp.innerHTML = `<li>${email}</li>`;
-            document.body.appendChild(emailStamp);
 
-        }
+        btn.addEventListener("click", function () {
 
-        
+            if (isGeneerate === true) {
+                
+
+            } else {
+                btn.innerHTML = "genera"
+                for (let i = 0; i < 10; i++) {
+                    const email = resp.data.response;
+                    let newEmail = [];
+                    newEmail.push(email);
+                    console.log(newEmail)
+                    const emailStamp = document.createElement("ul")
+                    emailStamp.innerHTML = `<li>${email}</li>`;
+                    document.body.appendChild(emailStamp);
+
+                }
+            }
+isGeneerate = !isGeneerate
+
+        })
 
 
     })
+
+
