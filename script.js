c890c92da5c5239fa51826a535f1ee4d79310a2f
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2025-11-15 00:12:23 -0800

    add email

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("ciao")
+const apriUrlapi = "https://flynn.boolean.careers/exercises/api/random/mail"
+// console.log("ciao")
+const titleElem = document.querySelector("h1")
+axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(function (resp) {
+        let email = resp.data.response;
+        titleElem.innerHTML = `${email}`
+        let newEmail = [];
+        
+        for(let i = 0; i < 10; i++){
+            
+            newEmail.push(email);
+            console.log(newEmail)
+            const emailStamp = document.createElement("ul")
+            emailStamp.innerHTML = `<li>${email}</li>`;
+            document.body.appendChild(emailStamp);
+
+        }
+
+        
+
+
+    })
